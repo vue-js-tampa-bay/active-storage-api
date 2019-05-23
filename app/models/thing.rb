efe7544159070e90a21155bcd9324193642138df
benj <benj@knowbe4.com>
2019-05-23 09:39:27 -0400

    active storage add, model created

---
+++ app/models/thing.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: things
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Thing < ApplicationRecord
+  has_one_attached :image
+  validates :name, presence: true
+end
